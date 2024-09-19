c8b9d065779712bbb869253aa7601a58c1f1e39c
roee ltmanovich <roeeli770@gmail.com>
2024-09-19 17:52:50 +0300

    עיצוב טיפה םחות מזוויע לעמוד

--- app.ts
+++ app.ts
@@ -4,17 +4,15 @@ const BASE_URl: string = "https://nbaserver-q21u.onrender.com/api/filter";
 const bottuneGetPlayer: HTMLParagraphElement =
   document.querySelector(".cerch-player")!;
 
-
 bottuneGetPlayer.addEventListener("click", async (e): Promise<void> => {
-  const table: HTMLTableElement = document.querySelector(".table")!
+  const table: HTMLTableElement = document.querySelector(".table")!;
   const player: Plaier = searchParameters();
   const players: Plaier[] | undefined = await getAllPlaierFromApi(player);
-  clearTabal()
-  for(const player of players!){
-    const htmlRow: HTMLTableRowElement = creatRoeHtml(player)
-    table.appendChild(htmlRow)
+  clearTabal();
+  for (const player of players!) {
+    const htmlRow: HTMLTableRowElement = creatRoeHtml(player);
+    table.appendChild(htmlRow);
   }
-  
 });
 
 const searchParameters = (): Plaier => {
@@ -34,7 +32,9 @@ const searchParameters = (): Plaier => {
   return player;
 };
 
-const getAllPlaierFromApi = async (plaier: Plaier): Promise<Plaier[] | undefined> => {
+const getAllPlaierFromApi = async (
+  plaier: Plaier
+): Promise<Plaier[] | undefined> => {
   try {
     const res: Response = await fetch(BASE_URl, {
       method: "POST",
@@ -42,104 +42,93 @@ const getAllPlaierFromApi = async (plaier: Plaier): Promise<Plaier[] | undefined
       headers: {
         "Content-Type": "application/json",
       },
-    })
+    });
     return await res.json();
-
   } catch (err) {
     console.log(err);
   }
-}
+};
 
-const clearTabal = ():void =>{
-   const listRow: NodeListOf<Element> = document.querySelectorAll(".delet-row")
-   for(const row of listRow){
-         row.remove()
-   }
-}
+const clearTabal = (): void => {
+  const listRow: NodeListOf<Element> = document.querySelectorAll(".delet-row");
+  for (const row of listRow) {
+    row.remove();
+  }
+};
 
-const creatRoeHtml = (player:Plaier):HTMLTableRowElement =>{
+const creatRoeHtml = (player: Plaier): HTMLTableRowElement => {
+  const row = document.createElement("tr");
 
-    const row = document.createElement("tr")
+  row.classList.add("delet-row");
 
-    row.classList.add("delet-row")
+  const tdPlayer: HTMLTableCellElement = document.createElement("td");
+  tdPlayer.textContent = player.playerName!;
+  const Postion: HTMLTableCellElement = document.createElement("td");
+  Postion.textContent = player.position;
+  const Points: HTMLTableCellElement = document.createElement("td");
+  Points.textContent = player.points.toString();
+  const FG: HTMLTableCellElement = document.createElement("td");
+  FG.textContent = player.twoPercent.toString();
+  const pPercent: HTMLTableCellElement = document.createElement("td");
+  pPercent.textContent = player.threePercent.toString();
+  const Action: HTMLTableCellElement = document.createElement("td");
+  const p: HTMLParagraphElement = document.createElement("p");
+  p.textContent = `add ${player.playerName} ro Current Team`;
+  p.addEventListener("click", (e) => {
+    CheckingWhereToAdd(player);
+  });
+  Action.appendChild(p);
 
-    const tdPlayer: HTMLTableCellElement = document.createElement("td")
-    tdPlayer.textContent = player.playerName!
-    const Postion: HTMLTableCellElement = document.createElement("td")
-    Postion.textContent = player.position
-    const Points: HTMLTableCellElement = document.createElement("td")
-    Points.textContent = player.points.toString()
-    const FG: HTMLTableCellElement = document.createElement("td")
-    FG.textContent = player.twoPercent.toString()
-    const pPercent: HTMLTableCellElement = document.createElement("td")
-    pPercent.textContent = player.threePercent.toString()
-    const Action: HTMLTableCellElement = document.createElement("td")
-    const p: HTMLParagraphElement = document.createElement("p")
-    p.textContent = `add ${player.playerName} ro Current Team`
-    p.addEventListener("click",(e)=>{
-        CheckingWhereToAdd(player)
-        
-    })
-    Action.appendChild(p)
+  row.appendChild(tdPlayer);
+  row.appendChild(Postion);
+  row.appendChild(Points);
+  row.appendChild(FG);
+  row.appendChild(pPercent);
+  row.appendChild(Action);
 
-    row.appendChild(tdPlayer)
-    row.appendChild(Postion)
-    row.appendChild(Points)
-    row.appendChild(FG)
-    row.appendChild(pPercent)
-    row.appendChild(Action)
+  return row;
+};
 
-    return row
-}
+const CheckingWhereToAdd = (plaier: Plaier): void => {
+  switch (plaier.position) {
+    case "PG":
+      addPlaier(plaier, "PG");
+      break;
+    case "SG":
+      addPlaier(plaier, "SG");
+      break;
+    case "SF":
+      addPlaier(plaier, "SF");
+      break;
+    case "PF":
+      addPlaier(plaier, "PF");
+      break;
+    case "C":
+      addPlaier(plaier, "C");
+      break;
+  }
+};
 
-const CheckingWhereToAdd = (plaier:Plaier):void => {
-    switch(plaier.position){
-        case "PG" :
-            addPlaier(plaier,"PG")
-            break;        
-        case "SG" :
-            addPlaier(plaier,"SG")
-            break;
-        case "SF":
-            addPlaier(plaier,"SF")
-            break
-        case "PF":
-            addPlaier(plaier,"PF")
-            break;
-        case "C":
-            addPlaier(plaier,"C")
-            break;
-    }
+const addPlaier = (plaier: Plaier, Tagclass: string): void => {
+  const divInformtion = document.querySelector("." + Tagclass);
+  while (divInformtion!.children.length > 1) {
+    divInformtion?.lastChild?.remove();
+  }
 
-}
+  const name: HTMLParagraphElement = document.createElement("p");
+  name.textContent = plaier.playerName!;
+  const threePrecents: HTMLParagraphElement = document.createElement("p");
+  threePrecents.textContent = plaier.threePercent + "%";
+  const twoPrecents: HTMLParagraphElement = document.createElement("p");
+  twoPrecents.textContent = plaier.twoPercent + "%";
+  const points: HTMLParagraphElement = document.createElement("p");
+  points.textContent = plaier.points.toString();
 
-const addPlaier = (plaier:Plaier, Tagclass:string):void =>{
-
-    const divInformtion = document.querySelector("." + Tagclass)
-    while(divInformtion!.children.length > 1){
-        divInformtion?.lastChild?.remove()
-    }
-
-    const name: HTMLParagraphElement = document.createElement("p")
-    name.textContent = plaier.playerName!
-    const threePrecents: HTMLParagraphElement = document.createElement("p")
-    threePrecents.textContent = plaier.threePercent + "%"
-    const twoPrecents: HTMLParagraphElement = document.createElement("p")
-    twoPrecents.textContent = plaier.twoPercent + "%"
-    const points: HTMLParagraphElement = document.createElement("p")
-    points.textContent = plaier.points.toString()
-
-    divInformtion?.appendChild(name)
-    divInformtion?.appendChild(threePrecents)
-    divInformtion?.appendChild(twoPrecents)
-    divInformtion?.appendChild(points)
-
-}
-
-
-
-
-
+  divInformtion?.appendChild(name);
+  divInformtion?.appendChild(threePrecents);
+  divInformtion?.appendChild(twoPrecents);
+  divInformtion?.appendChild(points);
+};
 
 interface Plaier {
   position: string;
